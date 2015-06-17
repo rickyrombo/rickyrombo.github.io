37cbd28d3f3b8144aaed3f09013d4b11d61a1306
Marcus Pasell <marcus.pasell@huskers.unl.edu>
2015-06-17 02:10:52 -0700

    updated to old site

---
+++ js/nav.js
@@ -0,0 +1,29 @@
+define('nav', ['jquery', 'router'], function($, Router){
+    var refresh = function() {
+		console.log('refreshing', $('a[target!="_blank"]'));
+		$('a[target!="_blank"]').click(function(e){
+			e.preventDefault();
+			Backbone.history.navigate(e.target.getAttribute('href'), {trigger: true});
+			if ($(e.target).hasClass('push-state')){
+				$('.navbar-toggle').click();
+			}
+		});
+	};
+	$(document).ready(function(){
+        refresh();
+    });
+    $('.navbar-toggle').click(function(e){
+        e.preventDefault();
+        var target = $(this).attr('data-target');
+        if($(this).hasClass('collapsed')){
+            //$(target).css('height', 'auto');
+            //$(target).animate({height: '100%'});
+            $(target).addClass('rr-expanded')
+            $(this).removeClass('collapsed');
+        } else {
+            $(target).removeClass('rr-expanded');
+            $(this).addClass('collapsed');
+        }
+    });
+	return { refresh: refresh };
+});
---
+++ js/router.js
@@ -0,0 +1,49 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'app',
+	'nav',
+    'views/music-view',
+    'views/favorites-view',
+    'views/followings-view',
+    'views/news-view',
+    'views/about-view',
+    'views/post-view',
+], function($, _, Backbone, App, nav, MusicView, FavoritesView, FollowingsView, NewsView, AboutView, PostView){
+        var musicView = new MusicView();
+        var favoritesView = new FavoritesView();
+        var followingsView = new FollowingsView();
+        var newsView = new NewsView();
+        var aboutView = new AboutView();
+		console.log(nav);
+        var AppRouter = Backbone.Router.extend({
+            routes: {
+                'music' : 'renderMusic',
+                'favorites' : 'renderFavorites',
+                'followings' : 'renderFollowings',
+                '*action' : 'renderPage'
+            },
+            renderMusic: function() {
+                musicView.render();
+            },
+            renderFavorites: function() {
+                favoritesView.render();
+            },
+            renderFollowings: function() {
+                followingsView.render();
+            },
+            renderPage: function(action) {
+				if (action === null) {
+					action = '';
+				}
+                $.get('/' + action).done(function(contents) {
+					$('#content').html($(contents).find('#content'));
+					nav.refresh();
+				});
+            },
+            init : function(){ Backbone.history.start({pushState: true, root: "/"}); }
+        });
+        return AppRouter;
+    }
+)
