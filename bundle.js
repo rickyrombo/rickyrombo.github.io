30867066dbab0a8c212b43c0ade60394cf2a3b57
Marcus Pasell <rickyrombo@gmail.com>
2016-11-10 01:29:36 -0600

    rebuilding site Thu, Nov 10, 2016  1:29:36 AM

--- bundle.js
+++ bundle.js
@@ -23577,7 +23577,7 @@
 
 	_soundcloud2.default.initialize({
 	    client_id: '4790864defb6a0d7eb3017d49a31b273',
-	    redirect_uri: window.location.protocol + '//' + window.location.host + '/callback'
+	    redirect_uri: window.location.protocol + '//rickyrombo.github.io/callback'
 	});
 
 	module.exports = _soundcloud2.default;
